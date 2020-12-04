39eeab54266f92241b7cadb1a09faf25162fdf60
James Ashford <james.ashford@bnc.ox.ac.uk>
2020-12-04 17:00:54 +0000

    Generalised result to "n" numbers - still very brute force though!

--- 1_report_repair.py
+++ 1_report_repair.py
@@ -1,18 +1,14 @@
 # Find the numbers in the list that sum to 2020, and get the product
 from itertools import product
 from timeit import timeit
+from functools import reduce
+from operator import mul
 
 
-def target_sum_product_two(number_list, target_sum=2020):
+def target_sum_product(number_list, target_sum=2020, n = 2):
     # Find three numbers that sum to 2020, and print product
-    for (n1, n2) in product(number_list, number_list): 
-        if n1+n2 == target_sum: return(n1*n2)
-
-
-def target_sum_product_three(number_list, target_sum=2020):
-    # Find three numbers that sum to 2020, and print product
-    for (n1, n2, n3) in product(number_list, number_list, number_list): 
-        if n1+n2+n3 == target_sum: return(n1*n2*n3)
+    for vals in product(number_list, repeat = n): 
+        if sum(vals) == target_sum: return(reduce(mul, vals, 1))
 
 
 # Read in expenses
@@ -20,12 +16,11 @@ with open("rsc/1_expenses.txt") as expenses_file:
     expenses = [int(n) for n in expenses_file.read().split("\n")]
 
 # Problem Solutions
-print(f"Part 1: {target_sum_product_two(expenses)}")
-print(f"Part 2: {target_sum_product_three(expenses)}")
+print(f"Part 1: {target_sum_product(expenses, n = 2)}")
+print(f"Part 2: {target_sum_product(expenses, n = 3)}")
 
 # Timing; for fun!
 n_runs = 10
-setup = """from __main__ import target_sum_product_two, expenses"""
-run = """target_sum_product_two(expenses)"""
-a = timeit(setup=setup, stmt=run, number=n_runs)/n_runs
-print(a)
+setup = """from __main__ import target_sum_product, expenses"""
+run = """target_sum_product(expenses, n=3)"""
+print(timeit(setup=setup, stmt=run, number=n_runs)/n_runs)
