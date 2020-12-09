7f2e092ca338095b07ee1f556edd56abd464206d
Max Antson <max@feebris.com>
2020-12-09 14:45:08 +0100

    remove unnecessary dependencies

--- ma/scripts/day_9.py
+++ ma/scripts/day_9.py
@@ -2,18 +2,16 @@ from itertools import permutations
 import os
 import pathlib
 
-import numpy as np
-import pandas as pd
-
 
 # Get data
 os.chdir(pathlib.Path(__file__).parent)
-nums = np.array(pd.read_csv("../data/day_9.txt", header=None)[0])
+with open("../data/day_9.txt") as f:
+    nums = [int(line) for line in f.readlines()]
 
 
 # Part 1
 for i in range(25, len(nums)):
-    if np.all([j + k != nums[i] for j, k in permutations(nums[i - 25:i], 2)]):
+    if all([j + k != nums[i] for j, k in permutations(nums[i - 25:i], 2)]):
         print("Day 9, Part 1:", nums[i])
         break
 
@@ -21,8 +19,8 @@ for i in range(25, len(nums)):
 # Part 2
 bad_num = nums[i]
 for k in range(2, len(nums)):
-    sums = [np.sum(nums[i: i + k]) for i in range(len(nums) - k)]
+    sums = [sum(nums[i: i + k]) for i in range(len(nums) - k)]
     if bad_num in sums:
-        idx = np.where(np.array(sums) == bad_num)[0][0]
+        idx = sums.index(bad_num)
         print("Day 9, Part 2:", max(nums[idx:idx + k]) + min(nums[idx:idx + k]))
         break
