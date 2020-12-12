070a3c0218327bc05f2124ace147380d36350681
Sam Morrell <samuelmorrell@gmail.com>
2020-12-12 00:28:34 +0000

    Working solution for part 1 of day 1.

---
+++ day1/src/main.rs
@@ -0,0 +1,45 @@
+/// A Rust implementation of a soluation for Day 1 of Advent of Code 2020.
+/// https://adventofcode.com/2020/day/1
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::result::Result;
+
+const TARGET: u32 = 2020;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = "input.txt";
+    // Read the contents of the input file here
+    match get_input(path) {
+        Ok(input) => {
+            
+            'outer: for i1 in 0..input.len() {
+                for i2 in 0..input.len() {
+                    if input[i1] + input[i2] == TARGET && i1 != i2 {
+                        println!("Found i1={} and i2={}. ", i1, i2);
+                        println!("Result: {}", input[i1] * input[i2]);
+                        break 'outer;
+                    }
+                }
+            }
+
+            Ok(())
+        }
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+/// Gets the input list of numbers from a file.
+pub fn get_input(filepath: &str) -> Result<Vec<u32>, std::io::Error> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+    reader
+        .lines()
+        .map(|txt| {
+            txt.and_then(|txt| {
+                txt.parse()
+                    .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))
+            })
+        })
+        .collect()
+}
